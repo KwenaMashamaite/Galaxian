48215bb6e65c57d797a91801c7313e31a80b2399
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2022-04-09 04:29:38 +0200

    Hide windows console window in release build

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include "GameEngine.h"
 
+#ifdef NDEBUG
+    #include "windows.h"
+#endif
+
 int main() {
+
+#ifdef NDEBUG
+    // Hide console window in release mode
+    HWND hwnd = GetConsoleWindow();
+    ShowWindow(hwnd, SW_HIDE);
+#endif
+
     GameEngine engine;
     engine.init();
     engine.run();
